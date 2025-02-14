fbbcb28b690d022efa493992182b5b507679aeed
Marano <matheusmmv14@gmail.com>
2025-02-14 17:53:53 -0300

    First commit, initial back-end

---
+++ Back-end/src/main/java/io/github/edmitsu/autocad/autocad/AutocadApplication.java
@@ -0,0 +1,13 @@
+package io.github.edmitsu.autocad.autocad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocadApplication.class, args);
+	}
+
+}
---
+++ Back-end/src/test/java/io/github/edmitsu/autocad/autocad/AutocadApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.edmitsu.autocad.autocad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
